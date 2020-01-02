7156c8e3e1bf4f1fe540b1718a8475c3400798f1
jody <jodyjamesw@yahoo.com>
2020-01-02 18:34:49 +0000

    Working in windows not working in docker

--- test_script.py
+++ test_script.py
@@ -5,6 +5,11 @@ A simple selenium test example
 import unittest
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+import pyodbc
+import time
+import datetime
+import csv
+import sys
 
 
 class TestTemplate(unittest.TestCase):
@@ -17,7 +22,8 @@ class TestTemplate(unittest.TestCase):
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-gpu')
         self.driver = webdriver.Chrome(
-            options=chrome_options, executable_path="C:\\Projects\\PersonalGit\\AggScrapes\\chromedriver.exe")
+            options=chrome_options)
+
         self.driver.implicitly_wait(10)
 
     def tearDown(self):
@@ -31,10 +37,73 @@ class TestTemplate(unittest.TestCase):
             el = self.driver.find_element_by_id(
                 'Market_HomePage_HomepageHeroBlock_HomeInsurance')
             el.click()
-            
+            print('Click on quote button done move to write to db')
+            time.sleep(30)
+            print(pyodbc.drivers())
+            self.readCSV()
+            self.writeData('data')
 
         except NoSuchElementException as ex:
             self.fail(ex.msg)
+
+    def readCSV(self):
+        ifile = open('/code/FixedCTM_segments_Claims.csv')
+        reader = csv.reader(ifile)
+
+        for row in reader:
+            print(row)
+        ifile.close()
+
+    def writeData(self, data):
+        print(pyodbc.drivers())
+        # print(datetime.datetime.now())
+        # time.sleep(240)
+        # print('finished sleep for 240')
+        # print(datetime.datetime.now())
+
+        conn = pyodbc.connect(
+            "DRIVER={SQL Server};SERVER=srv-sql-08;UID=MysteryShopping;PWD=S9htyamncKrjVXHN;DATABASE=MysteryShopping")
+
+        cursor = conn.cursor()
+        cursor.execute(
+            'SELECT * FROM MysteryShopping.dbo.MysteryShopping_Test')
+
+        cursor.execute('''
+                INSERT INTO MysteryShopping.dbo.MysteryShopping_Test (Agreement_ID,
+                Test_Case,
+                Date_Captured,
+                Aggregator,
+                Segment,
+                Periodicity,
+                Competitor,
+                Price,
+                RankInQuote,
+                Email,
+                Customer_Id,
+                Focus,
+                Segment_Name_for_DW)      
+                VALUES
+                (1,
+                55,
+                getdate(),
+                'Aggregator',
+                'Segment',
+                'Periodicity',
+                'Competitor',
+                55.55,
+                100,
+                'j@j.com',
+                1,
+                'Focus',
+                'Segment_Name_for_DW')
+                ''')
+        conn.commit()
+
+        # print('writeData')
+        # print(data)
+        # print('sleep for 240')
+        # print(datetime.datetime.now())
+        # time.sleep(240)
 
 
 if __name__ == '__main__':
